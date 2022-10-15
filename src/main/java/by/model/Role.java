c11f97acdb28c622e1f434022a737a7e1e9ab204
Kate <2003katy2003@mail.ru>
2022-10-15 21:13:31 +0300

    model

---
+++ src/main/java/by/model/Role.java
@@ -0,0 +1,20 @@
+package by.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "Role")
+public class Role extends BaseEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private List<Account> accounts;
+}
